3038314c7d29704c911b1dba1ebc7770f9b8899e
Kashif <kashifdeshmukh786@gmail.com>
2025-05-01 21:15:23 +0530

    Latest changes

--- App.tsx
+++ App.tsx
@@ -61,6 +61,25 @@ function AccountStack() {
   );
 }
 
+const tabScreenOptions = ({route}: {route: any}) => ({
+  headerShown: false,
+  tabBarIcon: ({color, size}: {color: string; size: number}) => {
+    let iconName = 'home';
+    if (route.name === 'HomeTab') {
+      iconName = 'home';
+    } else if (route.name === 'Pharmacy') {
+      iconName = 'medkit';
+    } else if (route.name === 'Cart') {
+      iconName = 'cart';
+    } else if (route.name === 'Account') {
+      iconName = 'person';
+    } else if (route.name === 'Blog') {
+      iconName = 'book';
+    }
+    return <Icon name={iconName} size={size} color={color} />;
+  },
+});
+
 const App = () => {
   return (
     <Provider store={store}>
@@ -74,18 +93,7 @@ const App = () => {
             />
             <Tab.Navigator
               initialRouteName="HomeTab"
-              screenOptions={({route}) => ({
-                headerShown: false,
-                tabBarIcon: ({color, size}) => {
-                  let iconName = 'home';
-                  if (route.name === 'HomeTab') iconName = 'home';
-                  else if (route.name === 'Pharmacy') iconName = 'medkit';
-                  else if (route.name === 'Cart') iconName = 'cart';
-                  else if (route.name === 'Account') iconName = 'person';
-                  else if (route.name === 'Blog') iconName = 'book';
-                  return <Icon name={iconName} size={size} color={color} />;
-                },
-              })}>
+              screenOptions={tabScreenOptions}>
               <Tab.Screen
                 name="HomeTab"
                 component={HomeStack}
