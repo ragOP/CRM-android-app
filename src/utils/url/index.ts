1990594d416c5978801d6c122befeed028d5afa8
Kashif <kashifdeshmukh786@gmail.com>
2025-05-08 12:47:18 +0530

    Changed to live url

--- src/utils/url/index.ts
+++ src/utils/url/index.ts
@@ -1,8 +1,8 @@
 import {isDev} from '../is_dev';
 
-// export const BACKEND_URL = isDev()
-//   ? 'https://2c66-84-247-129-99.ngrok-free.app'
-//   : 'https://2c66-84-247-129-99.ngrok-free.app';
 export const BACKEND_URL = isDev()
-  ? 'http://192.168.0.103:8000'
-  : 'http://localhost:8000';
+  ? 'https://2c66-84-247-129-99.ngrok-free.app'
+  : 'https://2c66-84-247-129-99.ngrok-free.app';
+// export const BACKEND_URL = isDev()
+//   ? 'http://192.168.0.103:8000'
+//   : 'http://localhost:8000';
