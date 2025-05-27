cb04de113eea5b387849d068d9c1a80c5603ccee
Kashif <kashifdeshmukh786@gmail.com>
2025-05-27 19:21:00 +0530

    URL Changed

--- src/utils/url/index.ts
+++ src/utils/url/index.ts
@@ -1,5 +1,8 @@
 import {isDev} from '../is_dev';
 
 export const BACKEND_URL = isDev()
-  ? 'http://10.0.2.2:8000'
+  ? 'https://393d-84-247-129-99.ngrok-free.app'
   : 'https://393d-84-247-129-99.ngrok-free.app';
+
+
+// http://10.0.2.2:8000
